070c58f96a2c2828fe0b002c22d8eb43326efb21
Billy Huang <10646003@ntub.edu.tw>
2020-03-14 16:30:54 +0800

    Creating Models

---
+++ swift-coffee/Models/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  swift-coffee
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
