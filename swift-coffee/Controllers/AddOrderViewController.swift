118c8fa8983fe0b3a039d0351a1494c178a011b0
Billy Huang <10646003@ntub.edu.tw>
2020-03-14 15:35:05 +0800

    Creating the User Interface

---
+++ swift-coffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddOrderViewController.swift
+//  swift-coffee
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ swift-coffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OrdersTableViewController.swift
+//  swift-coffee
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
